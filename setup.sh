a0caabf2b8c84b645d41951d5de7560db76475a9
Bartosz Galek <bartosz.galek@allegro.pl>
2019-01-14 16:22:12 +0100

    brew install pyenv added, lockfile created

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
+brew install pyenv
 pyenv install 3.6.5
 pipenv install --python ~/.pyenv/versions/3.6.5/bin/python3
 pipenv shell
